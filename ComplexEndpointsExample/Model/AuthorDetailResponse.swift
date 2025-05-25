e4d27027d05a4affbeefebbeea36f77d796a941b
Russell Gordon <rgordon@lcs.on.ca>
2025-05-25 19:33:16 -0400

    First stab at writing a model to decode a response with details about an individual author into.

---
+++ ComplexEndpointsExample/Model/AuthorDetailResponse.swift
@@ -0,0 +1,35 @@
+//
+//  AuthorDetailResponse.swift
+//  ComplexEndpointsExample
+//
+//  Created by Russell Gordon on 2025-05-25.
+//
+
+import Foundation
+
+struct AuthorDetailResponse: Codable {
+    
+    // MARK: Stored properties
+    let name: String
+    let links: [AuthorLinkResponse]
+    let fullName: String?
+    let birthDate: String?
+    let biography: String?
+    
+    // MARK: Enumeration(s)
+    enum CodingKeys: String, CodingKey {
+        
+        case name
+        case links
+        case fullName = "fuller_name"
+        case birthDate = "birth_date"
+        case biography = "bio"
+    }
+}
+
+struct AuthorLinkResponse: Codable {
+    
+    // MARK: Stored properties
+    let title: String
+    let url: String
+}
