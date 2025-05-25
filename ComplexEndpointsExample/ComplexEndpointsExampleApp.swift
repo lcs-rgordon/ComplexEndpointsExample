0f6616fa5a43df439ec30d4e982a20ad14f68c7b
Russell Gordon <rgordon@lcs.on.ca>
2025-05-25 09:32:28 -0400

    Initial Commit

---
+++ ComplexEndpointsExample/ComplexEndpointsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexEndpointsExampleApp.swift
+//  ComplexEndpointsExample
+//
+//  Created by Russell Gordon on 2025-05-25.
+//
+
+import SwiftUI
+
+@main
+struct ComplexEndpointsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
