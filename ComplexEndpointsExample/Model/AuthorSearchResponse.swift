21ff3c9ce2d585fc41a59220578582480e09d181
Russell Gordon <rgordon@lcs.on.ca>
2025-05-25 10:19:26 -0400

    First shot at constructing a model for looking up a list of authors based on a provided search string.

---
+++ ComplexEndpointsExample/Model/AuthorSearchResponse.swift
@@ -0,0 +1,44 @@
+//
+//  AuthorSearchResponse.swift
+//  ComplexEndpointsExample
+//
+//  Created by Russell Gordon on 2025-05-25.
+//
+
+import Foundation
+
+struct AuthorSearchResponse: Codable {
+    
+    // MARK: Stored properties
+    let hits: Int
+    let docs: [AuthorDocumentationResponse]
+
+    // MARK: Enumeration(s)
+    enum CodingKeys: String, CodingKey {
+        
+        case hits = "numFound"
+        case docs
+    }
+
+}
+
+struct AuthorDocumentationResponse: Identifiable, Codable {
+    
+    // MARK: Stored properties
+    let id: String
+    let name: String
+    let birthDate: String
+    let topWork: String
+    let workCount: Int
+    
+    // MARK: Enumeration(s)
+    enum CodingKeys: String, CodingKey {
+        
+        case id = "key"
+        case name
+        case birthDate = "birth_date"
+        case topWork = "top_work"
+        case workCount = "work_count"
+        
+    }
+}
