16e7a57a304aa7cf9a451e23c4f9cafe4b3a5ed4
Russell Gordon <rgordon@lcs.on.ca>
2025-05-25 09:36:13 -0400

    Initial project organization.

--- ComplexEndpointsExample/ComplexEndpointsExampleApp.swift
+++ ComplexEndpointsExample/ComplexEndpointsExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ComplexEndpointsExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthorSearchView()
         }
     }
 }
---
+++ ComplexEndpointsExample/Views/AuthorSearchView.swift
@@ -0,0 +1,21 @@
+//
+//  AuthorSearchView.swift
+//  ComplexEndpointsExample
+//
+//  Created by Russell Gordon on 2025-05-25.
+//
+
+import SwiftUI
+
+struct AuthorSearchView: View {
+    var body: some View {
+        VStack {
+            Text("This will present a search interface to look up authors, eventually.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AuthorSearchView()
+}
