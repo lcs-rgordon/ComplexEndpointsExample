2e6b8e750dd4c4abb86f6d481d078d52b23f9449
Russell Gordon <rgordon@lcs.on.ca>
2025-05-25 10:57:08 -0400

    Initial view that shows a helpful message when there are not yet any results to show.

--- ComplexEndpointsExample/Views/AuthorSearchView.swift
+++ ComplexEndpointsExample/Views/AuthorSearchView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct AuthorSearchView: View {
+    
+    // MARK: Stored properties
+    
+    // Establish a connection to the view model for this view
+    @State private var viewModel = AuthorSearchViewModel()
+    
+    // Hold the text typed into the search field
+    @State private var searchText = ""
+    
+    // MARK: Computed properties
     var body: some View {
-        VStack {
-            Text("This will present a search interface to look up authors, eventually.")
+        NavigationStack {
+            VStack {
+                // Is there a response yet, from which to show results?
+                if let currentResponse = viewModel.currentResponse {
+                    
+                    // Yes, so show those results
+                    Text("To be implemented")
+                    
+                } else {
+                    
+                    // No, so show appropriate message
+                    ContentUnavailableView(
+                        "No results",
+                        systemImage: "person.fill.questionmark",
+                        description: Text("Provide an author name to get started")
+                    )
+                    
+                }
+            }
+            .searchable(text: $searchText, prompt: "Enter name here")
+            .navigationTitle("Author Search")
         }
-        .padding()
     }
 }
 
